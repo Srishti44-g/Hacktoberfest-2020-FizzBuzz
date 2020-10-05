430e8061b037ded1efaa87e89e134894e6f2ec26
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2020-10-05 19:30:25 +0530

    Update FizzBuzz -Main_Ternary_Operator.java

--- Java/FizzBuzz -Main_Ternary_Operator.java
+++ Java/FizzBuzz -Main_Ternary_Operator.java
@@ -5,6 +5,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-	    for(int i = 1; i <= 100; i++) System.out.println((i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i));
+	    for(int i = 1; i <= 100; i++)
+            System.out.println((i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i));
 	}
 }
